962897e2fedb4a52730dd94f247d0a76915af9bb
pujitsingh-oss <pujit.singh@phonepe.com>
2025-09-02 23:55:55 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import MerchantCheckin from "./pages/MerchantCheckin";
 import PracticeMode from "./pages/PracticeMode";
 import SupervisorDashboard from "./pages/SupervisorDashboard";
+import Navbar from "./components/Navbar";
 
 function App() {
-
+  return (
     <div>
       <Navbar />
-
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/merchant" element={<MerchantCheckin />} />
-      <Route path="/practice" element={<PracticeMode />} />
-      <Route path="/dashboard" element={<SupervisorDashboard />} />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/merchant" element={<MerchantCheckin />} />
+        <Route path="/practice" element={<PracticeMode />} />
+        <Route path="/dashboard" element={<SupervisorDashboard />} />
       </Routes>
     </div>
   );
